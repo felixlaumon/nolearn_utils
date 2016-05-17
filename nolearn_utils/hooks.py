8e205a3163ca00192f0c01214c197709b05675a7
Alan Liu <alan@enlitic.com>
2016-05-16 18:44:18 -0700

    Fix typo

--- nolearn_utils/hooks.py
+++ nolearn_utils/hooks.py
@@ -22,7 +22,7 @@ class EarlyStopping(object):
         if current_valid < self.best_valid:
             self.best_valid = current_valid
             self.best_valid_epoch = current_epoch
-            self.best_weights = nn.get_all_param_values()
+            self.best_weights = nn.get_all_params_values()
         elif self.best_valid_epoch + self.patience <= current_epoch:
             print('Early stopping.')
             print('Best valid loss was {:.6f} at epoch {}.'.format(
